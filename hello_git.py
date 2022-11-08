beea7b5c0b1a556e0672920687209c9efb4a1716
YKH <a512611162@gmail.com>
2022-11-08 14:21:19 +0700

    Changes

--- hello_git.py
+++ hello_git.py
@@ -1,4 +1,13 @@
-print("Hello Git world!")
-print("Hello everyone.")
-print("123")
-print("123")
+import pygame
+pygame.init()
+# 1. 创建游戏主窗口
+
+screen = pygame.display.set_mode((480, 700))
+# 游戏界面大小
+
+# 游戏循环
+
+while True:
+    pass
+
+pygame.quit()
